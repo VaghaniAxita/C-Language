8cfcac702cd25691ceb5549ba45f791c5609f176
VaghaniAxita <vaghaniaxita23@gmail.com>
2024-03-19 12:26:15 +0530

    19/3/24 All 6 PR resubmit

--- PR 6 Filamen/1.c
+++ PR 6 Filamen/1.c
@@ -1,50 +1,55 @@
-#include <stdio.h>
+#include<stdio.h>
 
 main()
 {
-    char name[20], rev[20];
+    char name[20] , rev[20];
 
     printf("Enter any String: ");
-    scanf("%[^\n]", &name);
+    scanf("%[^\n]",&name);
 
-    int i, len = 0, n = 0;
+    int i ,len=0 ,n=0;
 
-    for (i = 0; name[i] != '\0'; i++)
-    {
+   
+    for(i=0;name[i]!='\0';i++){
         len++;
     }
-    for (i = 0; i < len; i++)
+     for(i=0;i<len;i++)
     {
-        if (name[i] >= 65 && name[i] <= 90)
-        {
-            name[i] = name[i] + 32;
-        }
+       if(name[i]>=65 && name[i]<=90){
+        name[i]=name[i]+32;
+       }
     }
-
-    for (i = 4; i >= 0; i--, n++)
+    // printf("Name\t : %s\n",name);
+    for(i=len-1;i>=0;i--,n++)
     {
-        rev[n] = name[i];
+        rev[n]=name[i];
+        //  printf("%c\n",rev[n]);
     }
-    int pal = 0;
-    for (i = 0; i < len; i++)
+    int pal=0;
+    for(i=0;i<len;i++)
     {
-        if (name[i] == rev[i])
+        if(name[i]==rev[i])
         {
-            pal = 1;
+            pal=1;
         }
-        else
-        {
-            pal = 0;
+        else{
+            pal=0;
             break;
         }
     }
+  
+         (pal==1)
+           ? printf("This String is Palindrome")
+           : printf("This String is not Palindrome");
 
-    if (pal == 1)
-    {
-        printf("\nThis String is Palindrome");
-    }
-    else
-    {
-        printf("This String is not Palindrome");
-    }
+    // if(pal==1)
+    // {
+    //     printf("\nThis String is Palindrome");
+    // }
+    // else
+    // {
+    //     printf("This String is not Palindrome");
+    // }
+    
+   
 }
